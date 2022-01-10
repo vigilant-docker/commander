cec0d6c2582d7125bf601a4554c0717f991a6d5c
Tadeu Barbosa <tadeufbarbosa@gmail.com>
2022-01-10 10:06:06 -0300

    test: ensure Container can receive a id and a name

---
+++ domain/entities/container.test.js
@@ -0,0 +1,7 @@
+describe('Container', () => {
+  test('ensure Container can receive id and name', () => {
+    const sut = new Container('any_id', 'id_name')
+    
+    expect(sut.name).toBe('any_name')
+  })
+});
